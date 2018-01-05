e319d6c67546b2b830389f8f3fce3e5923afefbc
Matt Larson <matt.a.larson@gmail.com>
2018-01-05 15:20:49 -0600

    first query for allLinks

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const client = new ApolloClient({
+    link: new
+HttpLink('https://api.graph.cool/simple/v1/cj5zozk22cj2n0104oqud5n90'),
+    cache: new InMemoryCache(),
+});
+
+const withApolloProvider = Comp => (
+    <ApolloProvider client={client}>{Comp}</ApolloProvider>
+  );
+
+  ReactDOM.render(
+    withApolloProvider(<App />),
+    document.getElementById('root'));
